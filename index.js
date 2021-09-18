941890f6541664db8aef2a333b8a739a38a8176e
ricardotn89 <ricardotn89@gmail.com>
2021-09-17 21:41:51 -0300

    ola

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 angular.module("app", [])
   .controller("MainCtrl", function ($scope) {
-    $scope.heading = "AngularJS";
+    $scope.heading = "AngularJS ols";
     $scope.text = "Never attribute to malice what can be adequately explained by stupidity.";
   });
